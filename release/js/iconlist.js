b2813afecc0b398ad9e922cd634123a3a60f7ca1
hihiboxhbtv <lemonsa22@hotmail.com>
2014-03-12 11:11:30 +0800

    release v1.5
    [Core] seperated core from crx
    [Added] Official link of Website, Facebook Page
    [Added] Dark/Light mode toogle
    [Improved] :spiderman: also known as :iamhay:
    [Minor] HihiBox scrollbar css

--- release/js/iconlist.js
+++ release/js/iconlist.js
@@ -1,10 +1,10 @@
 ﻿var infoName = 'Hihibox';
 var infoCredit = 'Designed by VannZic, Lemon';
-var infoVersion = 'v1.5.1';
-var infoLastUpdate = 'Last Updated on 2014-03-11';
+var infoVersion = 'v1.5';
+var infoLastUpdate = 'Last Updated on 2014-03-12';
 
 var defaultGenre = 'HKG';
-var listGenre = ['Recent','HKG','meme','pkmon','腦魔','禿伯','小丑','壯膽同學會'];
+var listGenre = ['Recent','HKG','HKG2','meme','pkmon','腦魔','禿伯','小丑','壯膽同學會'];
 var listParse = [];
 var listIcon = [].concat(
 
@@ -198,51 +198,51 @@ var listIcon = [].concat(
 	},
 	{	"code": ["[sosad369]"],
 		"icon": "http://img3.wikia.nocookie.net/__cb20060327033446/evchk/images/5/55/Sosad-369.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[sosad369ex]"],
 		"icon": "http://img2.wikia.nocookie.net/__cb20100901112145/evchk/images/b/b9/2010818205939156567860.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[369flowerface]"],
 		"icon": "http://img2.wikia.nocookie.net/__cb20060327040053/evchk/images/3/35/369face.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[ass369]"],
 		"icon": "http://img4.wikia.nocookie.net/__cb20060327040129/evchk/images/2/27/Ass3.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[assclown]"],
 		"icon": "http://img3.wikia.nocookie.net/__cb20060327040144/evchk/images/6/65/AssC.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[sosadbouncer]"],
 		"icon": "http://img2.wikia.nocookie.net/__cb20060327040204/evchk/images/b/bc/Bounsad.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[369sosad]"],
 		"icon": "http://img2.wikia.nocookie.net/__cb20060327040538/evchk/images/a/a9/Hahaha.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[sosadgood]"],
 		"icon": "http://img1.wikia.nocookie.net/__cb20060327040613/evchk/images/9/98/Sogood.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[clownbouncy]"],
 		"icon": "http://img2.wikia.nocookie.net/__cb20080620050648/evchk/images/a/a5/Soft_plastic.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[clownbouncer]"],
 		"icon": "http://img2.wikia.nocookie.net/__cb20080623080121/evchk/images/e/eb/Clownbouncer.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[369bye]"],
 		"icon": "http://img1.wikia.nocookie.net/__cb20080623075949/evchk/images/e/e9/369bye.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	},
 	{	"code": ["[clownbye]"],
 		"icon": "http://img1.wikia.nocookie.net/__cb20080803074219/evchk/images/5/55/CBye.gif",
-		"genre": ["HKG"]
+		"genre": ["HKG2"]
 	}
 ],
 /********************\
@@ -527,7 +527,7 @@ var listIcon = [].concat(
 		"genre": ["meme"],
 		"width": 80
 	},
-	{	"code": [":spiderman:"],
+	{	"code": [":spiderman:",":iamhay:"],
 		"icon": "http://ragegenerator.com/images/ragebuilder-faces/Miscellaneous/spiderpman.png",
 		"genre": ["meme"],
 		"width": 80
